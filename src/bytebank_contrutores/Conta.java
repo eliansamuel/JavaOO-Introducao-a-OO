965e8b5995b886d23dfb9b53c567adaf212c7938
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-01 20:09:19 -0300

    Creating bytebank_contrutores package and adding the Contas, Cliente and TestaGettersAndSetters classes

---
+++ src/bytebank_contrutores/Conta.java
@@ -0,0 +1,57 @@
+package bytebank_contrutores;
+
+public class Conta {
+	private double saldo;
+	private int agencia;
+	private int numero;
+	private Cliente titular;
+	
+	public void depositar(double valor) {
+		this.saldo += valor; // É igual a this.saldo = this.saldo + valor;
+		
+	}
+	
+	public boolean saca(double valor) { 
+		if (this.saldo >= valor) {
+			this.saldo -= valor; // É igual a this.saldo = this.saldo - valor;
+			return true;
+		} else {
+			return false;	
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.depositar(valor);
+			return true;
+		}
+		return false;
+	}
+	public double getSaldo(){
+		return this.saldo;
+	}
+	
+	public int getNumero() {
+		return this.numero;
+	}
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return this.agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public Cliente getTitular() {
+		return this.titular;
+	}
+
+	public void setTitular(Cliente titular) {
+		this.titular = titular;
+	}
+}
---
+++ src/bytebank_contrutores/TestaGettersAndSetters.java
@@ -0,0 +1,35 @@
+package bytebank_contrutores;
+
+public class TestaGettersAndSetters {
+	public static void main(String[] args) {
+		
+		Conta conta1 = new Conta();
+		
+		conta1.setNumero(1337);
+		conta1.depositar(150);
+		System.out.println("O número da conta é: " + conta1.getNumero());
+		
+		Cliente jubileu = new Cliente();
+		
+		jubileu.setNome("Jubileu dos Santos");
+		
+		// conta.titular = jubileu; -->  não podemos mais fazer isso pois os atributos estão privados.
+		
+		conta1.setTitular(jubileu); // --> maneira certa de atribuir um valor a um atributo.
+		
+		System.out.println(conta1.getTitular().getNome());
+		
+		conta1.getTitular().setProfissao("Programador");
+		// agora em duas linha
+		Cliente titularDaConta = conta1.getTitular();
+		titularDaConta.setProfissao("Programador");
+		
+		System.out.println(jubileu.getProfissao());
+		
+		// são o mesmo cliente, quer dizer apontam para o mesmo campo.
+		System.out.println(titularDaConta);
+		System.out.println(jubileu);
+		System.out.println(conta1.getTitular());
+		
+	}
+}
