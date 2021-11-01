7d1d5fd3b6c49c9bbbedf58698224e17dc9660a4
eliansamuel <eliansamuelsantos@gmail.com>
2021-11-01 15:59:35 -0300

    Adding content to the Conta class

--- src/bytebank/Conta.java
+++ src/bytebank/Conta.java
@@ -1,5 +1,33 @@
 package bytebank;
 
 public class Conta {
+	double saldo;
+	int agencia;
+	int numero;
+	String titular;
+	
+	public void depositar(double valor) {
+		this.saldo += valor; // É igual a this.saldo = this.saldo + valor;
+		
+	}
+	
+	public boolean saca(double valor) { 
+		if (this.saldo >= valor) {
+			this.saldo -= valor; // É igual a this.saldo = this.saldo - valor;
+			return true;
+		} else {
+			return false;	
+		}
+	}
+	
+	public boolean transfere(double valor, Conta destino) {
+		if (this.saldo >= valor) {
+			this.saldo -= valor;
+			destino.depositar(valor);
+			return true;
+		}
+		return false;
+	}
 
+	
 }
